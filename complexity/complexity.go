74a33dcf12c590118f8668fbc5a7b031639e1ff1
Masahiro Furudate <178inaba.git@gmail.com>
2023-04-16 03:54:58 +0900

    Add ast.Inspect

--- complexity/complexity.go
+++ complexity/complexity.go
@@ -4,5 +4,14 @@ import "go/ast"
 
 func Count(node ast.Node) int {
 	count := 1
+
+	ast.Inspect(node, func(node ast.Node) bool {
+		switch node.(type) {
+		case *ast.IfStmt:
+			count++
+		}
+		return true
+	})
+
 	return count
 }
