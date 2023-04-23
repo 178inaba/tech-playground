1a160bd6c3233e46f5e83386cd5250ba122b01de
Masahiro Furudate <178inaba.git@gmail.com>
2023-04-23 14:57:04 +0900

    Add fast-service/server

---
+++ fast-service/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+const maxSize = 26214400 // 25MB
+
+func main() {
+}
+
+func downloadHandler() http.HandlerFunc {
+	src := rand.NewSource(0)
+	return func(w http.ResponseWriter, r *http.Request) {
+		max, err := strconv.Atoi(r.URL.Query().Get("size"))
+		if err != nil {
+			max = maxSize
+		}
+
+		if _, err := io.CopyN(w, rand.New(src), int64(max)); err != nil {
+			log.Printf("Failed to write random data: %s", err)
+			return
+		}
+	}
+}
