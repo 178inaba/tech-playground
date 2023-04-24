8df88e6e6db3b836e804c819beefbc68b4347474
Masahiro Furudate <178inaba.git@gmail.com>
2023-04-25 01:17:19 +0900

    Add uploadHandler

--- fast-service/server/main.go
+++ fast-service/server/main.go
@@ -22,7 +22,21 @@ func downloadHandler() http.HandlerFunc {
 		}
 
 		if _, err := io.CopyN(w, rand.New(src), int64(max)); err != nil {
-			log.Printf("Failed to write random data: %s", err)
+			log.Printf("Failed to write random data: %v.", err)
+			return
+		}
+	}
+}
+
+func uploadHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		contentLength := r.ContentLength
+		if contentLength > maxSize {
+			contentLength = maxSize
+		}
+
+		if _, err := io.CopyN(io.Discard, r.Body, contentLength); err != nil {
+			log.Printf("Failed to write body: %v.", err)
 			return
 		}
 	}
