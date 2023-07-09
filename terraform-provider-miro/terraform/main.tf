d2b6dcfa63c47da5a832bb8197f5f8be18fd9d78
Masahiro Furudate <178inaba.git@gmail.com>
2023-07-09 16:29:22 +0900

    Fix tf file

--- terraform-provider-miro/terraform/main.tf
+++ terraform-provider-miro/terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    miro     = {
+    miro = {
       source = "178inaba/miro"
     }
   }
@@ -8,4 +8,9 @@ terraform {
 
 provider "miro" {
   access_token = var.access_token
+}
+
+resource "miro_board" "test" {
+  name        = "Test Board"
+  description = "My test board for Software Design"
 }
