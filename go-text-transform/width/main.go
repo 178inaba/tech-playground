c486d875bcf38c7012ccba773bde18ba2838c626
Masahiro Furudate <178inaba.git@gmail.com>
2023-08-17 16:10:04 +0900

    Fix go-text-transform/width

--- go-text-transform/width/main.go
+++ go-text-transform/width/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Println("p.Wide(), p.Narrow(), p.Folded(), p.Kind() ------------------------------------------------------------")
 	rs := []rune{'５', 'ｱ', 'ア', 'A', 'α'}
 	fmt.Println("rune\tWide\tNarrow\tFolded\tKind")
 	fmt.Println("--------------------------------------------------")
@@ -16,7 +17,23 @@ func main() {
 		fmt.Printf("%2c\t%2c\t%3c\t%3c\t%s\n", r, w, n, f, k)
 	}
 
-	for _, r := range width.Fold.String("５ｱアAα") {
+	fmt.Println("")
+	fmt.Println("width.Fold ------------------------------------------------------------")
+	for _, r := range width.Fold.String(string(rs)) {
+		p := width.LookupRune(r)
+		fmt.Printf("%c: %s\n", r, p.Kind())
+	}
+
+	fmt.Println("")
+	fmt.Println("width.Narrow ------------------------------------------------------------")
+	for _, r := range width.Narrow.String(string(rs)) {
+		p := width.LookupRune(r)
+		fmt.Printf("%c: %s\n", r, p.Kind())
+	}
+
+	fmt.Println("")
+	fmt.Println("width.Widen ------------------------------------------------------------")
+	for _, r := range width.Widen.String(string(rs)) {
 		p := width.LookupRune(r)
 		fmt.Printf("%c: %s\n", r, p.Kind())
 	}
