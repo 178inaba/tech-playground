8c11e6e08e0662c4c93095b52be17128955d8231
Masahiro Furudate <178inaba.git@gmail.com>
2023-10-01 01:14:05 +0900

    Add conversion logic to Upper.Transform

--- go-text-transform/upper-transformer/main.go
+++ go-text-transform/upper-transformer/main.go
@@ -11,8 +11,23 @@ func (Upper) Transform(dst, src []byte, atEOF bool) (nDst, nSrc int, err error) 
 		return
 	}
 
-	// TODO
-	return
+	for {
+		// srcをすべて処理し終えた、またはdstが足りなくなった場合。
+		if len(src) <= nSrc || len(dst) <= nDst {
+			return
+		}
+
+		// 小文字から大文字への変換。
+		if 'a' <= src[nSrc] && src[nSrc] <= 'z' {
+			dst[nDst] = src[nSrc] - 'a' + 'A'
+		} else {
+			dst[nDst] = src[nSrc]
+		}
+
+		// 処理したバイト数分だけ進める。
+		nSrc++
+		nDst++
+	}
 }
 
 func main() {
