0f53a08324606751d582c034f84f9481e6e5446b
Masahiro Furudate <178inaba.git@gmail.com>
2023-10-01 01:43:58 +0900

    [replace-transformer] Add replace logic

--- go-text-transform/replace-transformer/main.go
+++ go-text-transform/replace-transformer/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "golang.org/x/text/transform"
+import (
+	"bytes"
+
+	"golang.org/x/text/transform"
+)
 
 type Replacer struct {
 	transform.NopResetter
@@ -16,7 +20,30 @@ func (r *Replacer) Transform(dst, src []byte, atEOF bool) (nDst, nSrc int, err e
 		return
 	}
 
-	return
+	for {
+		// srcのnSrc番目からr.oldを探す。
+		i := bytes.Index(src[nSrc:], r.old)
+
+		// 見つからなかった場合。
+		if i == -1 {
+			// TODO
+			return
+		}
+
+		// 見つけたところまでをコピーして書き込む。
+		n := copy(dst[nDst:], src[nSrc:nSrc+i])
+		nDst += n
+		nSrc += n
+		if n < i {
+			err = transform.ErrShortDst
+			return
+		}
+
+		// 置換する文字をコピーして書き込む。
+		n = copy(dst[nDst:], r.new)
+		nDst += n
+		nSrc += len(r.old)
+	}
 }
 
 func main() {
