38344c86e83d9a765fc77d3bafc813f85610291f
Masahiro Furudate <178inaba.git@gmail.com>
2023-10-08 00:21:15 +0900

    Add `go-wasm/compress`

---
+++ go-wasm/compress/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+	"time"
+)
+
+func main() {
+}
+
+func Compress(src io.Reader) (io.Reader, error) {
+	var buf bytes.Buffer
+
+	zw := gzip.NewWriter(&buf)
+	zw.ModTime = time.Now()
+
+	if _, err := io.Copy(zw, src); err != nil {
+		return nil, err
+	}
+
+	if err := zw.Close(); err != nil {
+		return nil, err
+	}
+
+	return &buf, nil
+}
