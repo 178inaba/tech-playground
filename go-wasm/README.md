3e473ab45ea275f8cdf53c4ddb4ebed8e9767489
Masahiro Furudate <178inaba.git@gmail.com>
2023-10-08 00:00:34 +0900

    [go-wasm] Use `syscall/js`

--- go-wasm/README.md
+++ go-wasm/README.md
@@ -3,4 +3,7 @@
 ```console
 $ wget 'https://raw.githubusercontent.com/golang/go/master/misc/wasm/wasm_exec.js'
 $ GOOS=js GOARCH=wasm go build -o main.wasm
+$ serve -a :8000
 ```
+
+Access http://localhost:8000/ to check.
--- go-wasm/main.go
+++ go-wasm/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
-	fmt.Println("Hello, WebAssembly!")
+	// window Objectを取得する。
+	window := js.Global()
+	// document Objectを取得する。
+	document := window.Get("document")
+	// bodyのDOMを取得する。
+	body := document.Get("body")
+	// buttonのDOMを作成する。
+	btn := document.Call("createElement", "button")
+	// buttonに表示する文字を設定する。
+	btn.Set("textContent", "click me!")
+	// buttonにclickのEventListenerを設定する。
+	btn.Call("addEventListener", "click", js.FuncOf(func(js.Value, []js.Value) interface{} {
+		fmt.Println("Hello, WebAssembly!")
+		return nil
+	}))
+	// buttonをbody内に追加する。
+	body.Call("appendChild", btn)
+	// プログラムが終了しないようにするため待機する。
+	select {}
 }
