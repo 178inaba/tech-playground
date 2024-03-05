26805bc67075607447d00eb78f1ee4ae7e831e45
Masahiro Furudate <178inaba.git@gmail.com>
2024-03-06 02:02:40 +0900

    Add slack-user-belong-groups

---
+++ slack-user-belong-groups/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	flag.Parse()
+	userID := flag.Arg(0)
+	if userID == "" {
+		log.Fatal("User ID is required.")
+	}
+
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
+	groups, err := api.GetUserGroupsContext(context.Background(), slack.GetUserGroupsOptionIncludeUsers(true))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, g := range groups {
+		for _, u := range g.Users {
+			if u == userID {
+				fmt.Printf("%s: %s\n", g.Handle, g.Name)
+			}
+		}
+	}
+}
